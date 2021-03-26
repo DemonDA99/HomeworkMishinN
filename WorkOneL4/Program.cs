b68d8d226c2cf5fc649dfea5e5b3a60c22dd389f
DemonDA99 <nikita.mishin.1999@yandex.ru>
2021-03-26 09:06:16 +0300

    Lesson4

---
+++ WorkOneL4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace WorkOneL4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArray a = new MyArray(20, -10000, 10000);
+            Console.WriteLine(a.ToString());
+            Console.WriteLine(a.pairsOfNumbers());
+        }
+    }
+    class MyArray
+    {
+        int[] numbers;
+        
+        public MyArray(int n, int min, int max)
+        {
+            numbers = new int[n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+                numbers[i] = rnd.Next(min, max);
+        }
+        public override string ToString()
+        {
+            string s = "";
+            foreach (int v in numbers)
+                s = s + v + " ";
+            return s;
+        }
+        public int pairsOfNumbers()
+        {
+            int count = 0;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] % 3 == 0 | numbers[i + 1] % 3 == 0)
+                    count++;
+            }
+            return count;
+        }
+    }
+
+}
---
+++ WorkTwoL4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace WorkTwoL4
+{
+     public static class StaticClass
+    {
+        public static int[] Print(int n)
+        {
+            int[] numbers = new int[n];
+            Random rnd = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+                numbers[i] = rnd.Next(-10000, 10000);
+            return numbers;
+            
+        }
+        public static int[] MyArray()
+        {
+            StreamReader sr = new StreamReader("C:\\Users\\Owner\\Desktop\\C#\\Курсы\\HomeworkMishinN\\Data.txt");
+            int N = int.Parse(sr.ReadLine());
+            int[] masData = new int[N];
+            try
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    masData[i] = int.Parse(sr.ReadLine());
+                    Console.WriteLine(masData);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Файл отсутствует на диске!");
+            }
+            return masData;
+        }
+         public static void Main(string[] args)
+        {
+            int n = 20;
+            int[] mas = StaticClass.Print(n);
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", mas[i]);
+            }
+        }
+    }
+}
+   
