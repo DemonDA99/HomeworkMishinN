f64111500c45ae7c7a461775d17475dd23406e75
DemonDA99 <nikita.mishin.1999@yandex.ru>
2021-03-30 13:04:24 +0300

    Lesson5

---
+++ WorkOneL5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace WorkOneL5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите логин: ");
+            string login = Console.ReadLine() ;
+            {
+                if (login.Length < 2 || login.Length > 10)
+                {
+                    Console.WriteLine("Не коректная длина! ");
+                }
+                else
+                {
+                    if (char.IsDigit(login[0]))
+                    {
+                        Console.Write("Логин не может начинаться с цифры!");
+                    }
+                    else
+                    {
+                        for (int i = 0; i < login.Length; i++)
+                        {
+                            if (!(Char.IsDigit(login[i]) || login[i] >= 'a' && login[i] <= 'z' || login[i] >= 'A' && login[i] <= 'Z'))
+                            {
+                                Console.WriteLine("Недопустимые символы! ");
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Коректный логин!");
+                            }
+                        }
+                    }
+                }
+            }
+            Regex regex = new Regex("[a-z]{1}[a-z0-9]{1,8}");
+            Console.WriteLine(regex.IsMatch(login));
+        }
+    }
+}
---
+++ WorkTwoL5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace WorkTwoL5
+{
+    static class Message
+    {
+        public static string StringEnd()
+        {
+            Console.Write("Введите сообщение: ");
+            string str = Console.ReadLine();
+            
+
+            string regex = (@"\b\w{1,5}(?=\b)");
+
+            foreach (Match m in Regex.Matches(str, regex))
+            {
+                Console.WriteLine(m.Value);
+            }
+            return str;
+        }
+        public static string Delite()
+        {
+            const string Char = "f";
+            Console.Write("Введите сообщение: ");
+            string str = Console.ReadLine();
+
+            str = (new Regex("[a-z]+" + Char)).Replace(str, "").Trim();
+            Console.WriteLine(str);
+            return str;
+        }
+
+        static void Main(string[] args)
+        {
+            StringEnd();
+            Delite();
+            Console.WriteLine();
+        }
+    }
+}
